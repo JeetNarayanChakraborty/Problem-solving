c2e8219f43fc911741fac56a73aaf61e41e288f3
Jeet Narayan Chakraborty <68782923+JeetNarayanChakraborty@users.noreply.github.com>
2022-08-28 17:40:15 +0530

    Create Jumping on the clouds revisited.cpp

---
+++ Jumping on the clouds revisited.cpp
@@ -0,0 +1,3 @@
+/* A child is playing a cloud hopping game. In this game, there are sequentially numbered clouds that can be thunderheads or cumulus clouds. The character must jump from cloud to cloud until it reaches the start again.
+There is an array of clouds,  and an energy level . The character starts from  and uses  unit of energy to make a jump of size  to cloud . If it lands on a thundercloud, , its energy () decreases by  additional units. The game ends when the character lands back on cloud .
+Given the values of , , and the configuration of the clouds as an array , determine the final value of  after the game ends. */
